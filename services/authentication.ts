09394b5fd791a33512c55096b558fe6ccbd442d2
Mesach Venegas Santana <99062079+MesachVenegas@users.noreply.github.com>
2024-06-04 22:52:41 -0700

    changes: add comments and create
    add comments to Authentication method, and create a registerNewClient action

--- services/authentication.ts
+++ services/authentication.ts
@@ -1,23 +1,24 @@
 'use server'
 
+import bcrypt from 'bcryptjs';
+
 import { z } from 'zod';
 import { AuthError } from 'next-auth';
 
 import { signIn } from "@/auth";
 import prisma from "@/lib/prisma"
-import { LoginSchema } from '@/validations/auth';
-import { LoginProps } from "@/types/interface";
-import credentials from 'next-auth/providers/credentials';
-
+import { LoginSchema, RegisterSchema } from '@/validations/auth';
+import { UserProps } from "@/types/interface";
 
 
 /**
  * Authenticates a user in the database.
  *
- * @param {LoginSchema} data - The user credentials for authentication.
- * @returns {Promise<LoginProps | null>} Returns a message and the user data if authentication is successful.
-*/
-export default async function Authenticate(data: z.infer<typeof LoginSchema>) {
+ * @param {z.infer<typeof LoginSchema>} data - The user credentials for authentication.
+ * @returns {Promise<string | null>} Returns a message if authentication fails, or null if successful.
+ * @throws {Error} If an error occurs during authentication.
+ */
+export async function Authenticate(data: z.infer<typeof LoginSchema>) {
   try {
     await signIn("credentials", {
       ...data,
@@ -30,5 +31,36 @@ export default async function Authenticate(data: z.infer<typeof LoginSchema>) {
       return error.cause?.err?.message;
     }
     throw error
+  }
+}
+
+
+
+
+/**
+ * Registers a new user in the database.
+ *
+ * @param {z.infer<typeof RegisterSchema>} data - The user data to be registered.
+ * @return {Promise<User>} The newly created user.
+ */
+export async function registerNewuser(data: z.infer<typeof RegisterSchema>): Promise<UserProps>{
+  try {
+    const {username, email, password} = data
+
+    const hashedPassword = await bcrypt.hash(password, 10)
+
+    const newUser = prisma.user.create({
+      data :{
+        username,
+        email,
+        password: hashedPassword
+      }
+    })
+
+    const { password: _, ...user } = await newUser
+
+    return user;
+  } catch (error) {
+    throw error;
   }
 }

9be17de90c98e897c7c6196f6325001f10129559
Mesach Venegas Santana <99062079+MesachVenegas@users.noreply.github.com>
2024-06-05 04:57:23 -0700

    add: menu constant

---
+++ lib/constants.ts
@@ -0,0 +1,7 @@
+
+export const dashboarMenu = [
+  { name: "main", title: "principal" , href: "/dashboard" },
+  { name: "transactions", title: "transacciones", href: "/dashboard/transactions" },
+  { name: "budget", title: "presupuesto", href: "/dashboard/budget" },
+  { name: "others", title: "otros", href: "/dashboard/others" },
+]

b6e98cf7023a075721eacb71b12d00fd9204cafd
Fernando Alcázar Malfavón <4maferal@gmail.com>
2024-06-04 18:48:17 -0600

    Style: add bg-logo

--- app/auth/login/page.tsx
+++ app/auth/login/page.tsx
@@ -1,79 +1,54 @@
-import Image from 'next/image';
-import type { Metadata } from 'next';
+import Image from "next/image";
 
 import { faCircleUser } from "@fortawesome/free-solid-svg-icons";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 
 import Button from "@/components/ui/button";
 import Badge from "@/components/ui/badge-icon";
+import { faGithub, faLinkedin } from "@fortawesome/free-brands-svg-icons";
 import CustomLink from "@/components/ui/link";
-
-export const metadata: Metadata = {
-  title: "Iniciar sesión",
-  description: "Entre a la maravillo plataforma que te ayudara a mantener tu salud financiera y a llegar a fin de mes sin preocupaciones"
-}
+import LoginForm from "../_components/login-form";
 
 export default function LoginPage() {
-  return (
-    <main className="flex flex-col items-center justify-center w-full min-h-screen bg-gradient gap-10 p-10">
-        <Badge variant="success" size="lg">
-          <FontAwesomeIcon icon={faCircleUser} className="w-8 h-8" />
-        </Badge>
-        <h1 className="text-xl">Vamos a crearte una cuenta</h1>
-      <div className="flex flex-col justify-center items-center gap-6 p-10 w-full h-full">
-        {/* Proveedores de autentificación de terceros*/}
-        <div className="flex flex-col gap-6">
-          <Button className="shadow-none" variant="outline" size="lg">
-            <Image
-              src="/assets/google.svg"
-              width={18}
-              height={18}
-              alt="Google Icon"
-            />
-            Sign in with Google
-          </Button>
-          <Button className="shadow-none" variant="info" size="lg">
-            <Image
-              src="/assets/facebook.svg"
-              width={24}
-              height={24}
-              alt="Facebook Icon"
-            />
-            Login with Facebook
-          </Button>
-          <Button className="shadow-none" variant="black" size="lg">
-            <Image
-              src="/assets/apple.svg"
-              width={18}
-              height={18}
-              alt="Apple Icon"
-            />
-            Sign in with Apple
-          </Button>
-        </div>
-        {/* Separador */}
-        <div className="flex w-full max-w-sm items-center justify-center gap-6">
-          <hr className="w-full bg-slate-400 h-[2px]" />
-          o
-          <hr className="w-full bg-slate-400 h-[2px]" />
-        </div>
-        {/* Botón para inicio de sesión con correo y registro. */}
-        <div className="flex flex-col items-center gap-4 w-full">
-          <Button
-            size="wide"
-            className="justify-center shadow-none"
-            variant="info"
-          >
-            Inicia sesión
-          </Button>
-          <p className="flex w-full justify-center gap-4 text-xs">
-            ¿No tienes una cuenta?
-            <CustomLink href="/auth/register" className="">
-              Regístrate
-            </CustomLink>
-          </p>
-        </div>
-      </div>
-    </main>
-  );
+	return (
+		<main className="flex flex-col justify-center items-center min-h-screen gap-10 p-10 bg-logo bg-no-repeat bg-center bg[50%]">
+			<Badge variant="success" size="lg">
+				<FontAwesomeIcon icon={faCircleUser} className="w-full h-full" />
+			</Badge>
+			<h1 className="title-1">Iniciar Sesion</h1>
+			<div className="flex flex-col flex-grow justify-center items-center gap-6 w-full h-full">
+				{/* Formulario de login */}
+				<LoginForm />
+				{/* Separador */}
+				<div className="flex w-full max-w-[200px] items-center justify-center gap-6 font-semibold">
+					<hr className="w-full bg-slate-400 h-[2px]" />
+					o
+					<hr className="w-full bg-slate-400 h-[2px]" />
+				</div>
+				{/* Proveedores de autentificación de terceros*/}
+				<div className="flex gap-6">
+					<Button variant="outline" size="icon">
+						<Image
+							src="/assets/google.svg"
+							width={18}
+							height={18}
+							alt="Google Icon"
+						/>
+					</Button>
+					<Button variant="black" size="icon">
+						<FontAwesomeIcon icon={faGithub} className="w-full h-full" />
+					</Button>
+					<Button variant="info" size="icon">
+						<FontAwesomeIcon icon={faLinkedin} className="w-full h-full" />
+					</Button>
+				</div>
+				<p className="flex gap-2 text-sm">
+					¿Olvidaste tu contraseña?
+					<CustomLink href="#" className="text-primary">
+						Recuperar
+					</CustomLink>
+				</p>
+			</div>
+		</main>
+	);
 }

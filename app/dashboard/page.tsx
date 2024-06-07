c9da3191919e93abd8f59ab1cc50e8c8ed1aae1c
Mesach Venegas Santana <99062079+MesachVenegas@users.noreply.github.com>
2024-06-06 19:18:24 -0700

    add: dashboard routes

--- app/dashboard/page.tsx
+++ app/dashboard/page.tsx
@@ -4,12 +4,9 @@ export default function Dashboard() {
 
   return (
 		<section className="flex flex-col gap-6 w-full h-full border">
-      <Card >
-        Gráfico
-      </Card>
-      <Card className="min-h-80">
-        Calendario
-      </Card>
+			<Card className="p-4">
+			</Card>
+			<Card className="min-h-80">Calendario</Card>
 		</section>
 	);
 }

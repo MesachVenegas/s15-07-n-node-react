96002315af3531b5fa254cee5c903746692c2d24
Mesach Venegas Santana <99062079+MesachVenegas@users.noreply.github.com>
2024-06-06 19:18:01 -0700

    add: transactions page

---
+++ app/dashboard/transactions/page.tsx
@@ -0,0 +1,7 @@
+export default function Transactions() {
+  return (
+    <section className="flex flex-col justify-center items-center w-full min-h-screen gap-10 p-10">
+      <h1 className="title-1 text-center">Transactions</h1>
+    </section>
+  )
+}

d9abae1fa7d3c7cd7cdcb342408c72f5816c2c9f
Mesach Venegas Santana <99062079+MesachVenegas@users.noreply.github.com>
2024-06-07 20:17:40 -0700

    add: goal constant

--- lib/constants.ts
+++ lib/constants.ts
@@ -5,3 +5,11 @@ export const dashboarMenu = [
   { name: "budget", title: "presupuesto", href: "/dashboard/budget" },
   { name: "others", title: "otros", href: "/dashboard/others" },
 ]
+
+export const goal = {
+  control: 'control de mis gastos',
+  buy: 'juntar para comprar',
+  investment: 'ahorrar para invertir',
+  emergency: 'ahorrar para imprevistos',
+  other: 'otro'
+}

41565b3933af13db84fcd1024a84222e732245a0
Mesach Venegas Santana <99062079+MesachVenegas@users.noreply.github.com>
2024-06-13 01:30:21 -0700

    create: interface to global state to bills

--- types/bills.ts
+++ types/bills.ts
@@ -6,4 +6,30 @@ export interface BillProps {
   goalId: string;
   categoryId: string;
   createdDate?: Date;
-}``
+}
+
+export interface BillStateProps {
+  id?: string;
+  name: string;
+  amount: number;
+  categoryId: string;
+  icon: string;
+}
+
+export interface BillCategoryStateProps {
+  target: string;
+  id: string;
+  category: string;
+  icon: string;
+  variant: string;
+  bills: BillStateProps[];
+}
+
+export interface DefaultBillProps {
+  id: string;
+  name: string;
+  amount: number;
+  icon: string;
+  color: string;
+  categoryId: string;
+}

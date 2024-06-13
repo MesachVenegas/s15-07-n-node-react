2a12cdbc70135a9fd152ed988b65964307920ae1
Mesach Venegas Santana <99062079+MesachVenegas@users.noreply.github.com>
2024-06-13 01:30:46 -0700

    add: service to fetch default categories

--- services/category.ts
+++ services/category.ts
@@ -1,33 +1,63 @@
-import prisma from "@/lib/prisma";
-import { registerCategorySchema, RegisterCategory } from "@/validations/categorySchema";
+'use server'
+
 import { ZodError } from "zod"; // Import ZodError to handle Zod validation errors
 
-export async function registerCategory(
-  data: RegisterCategory
-): Promise<{ message: string; data: RegisterCategory }> {
-  try {
-    // Validate the input data using the schema
-    const validatedData = registerCategorySchema.parse(data);
+import prisma from "@/lib/prisma";
+import { CategoryProps } from "@/types/category";
+import { registerCategorySchema, RegisterCategory } from "@/validations/categorySchema";
+import { revalidatePath } from "next/cache";
 
-    // Create a new category in the database using Prisma
-    const newCategory = await prisma.category.create({
-      data: {
-        name: validatedData.name,
-        owner: validatedData.owner,
-      },
+
+/**
+ * Fetches default categories from the database.
+ *
+ * @return {Promise<{ message: string, data: CategoryProps[] }>} An object containing a message and an array of default categories.
+ */
+export async function FetchDefaultCategories(): Promise<{ message: string; data: CategoryProps[]; }>{
+  try {
+    const categories = await prisma.category.findMany({
+      where: {
+        default: true
+      }
     });
 
-    return {
-      message: "Category registered successfully",
-      data: newCategory,
-    };
-  } catch (error: unknown) { // Specify the error type as unknown
-    if (error instanceof ZodError) {
-      throw new Error(`Validation failed: ${error.errors.map(err => err.message).join(', ')}`);
-    } else if (error instanceof Error) {
-      throw new Error(`Registration failed: ${error.message}`);
-    } else {
-      throw new Error("An unknown error occurred.");
-    }
+    revalidatePath('/first-steps/bills')
+    return { message: "success", data: categories };
+  }
+  catch (error) {
+    throw error;
   }
 }
+
+
+// export async function registerCategory(
+//   data: CategoryProps
+// ): Promise<{ message: string; data: RegisterCategory }> {
+//   try {
+//     // Validate the input data using the schema
+//     const validatedData = registerCategorySchema.parse(data);
+
+//     // Create a new category in the database using Prisma
+//     const newCategory = await prisma.category.create({
+//       data: {
+//         name: validatedData.name,
+//         icon: '',
+//         default: false,
+//         owner: validatedData.owner,
+//       },
+//     });
+
+//     return {
+//       message: "Category registered successfully",
+//       data: newCategory,
+//     };
+//   } catch (error: unknown) { // Specify the error type as unknown
+//     if (error instanceof ZodError) {
+//       throw new Error(`Validation failed: ${error.errors.map(err => err.message).join(', ')}`);
+//     } else if (error instanceof Error) {
+//       throw new Error(`Registration failed: ${error.message}`);
+//     } else {
+//       throw new Error("An unknown error occurred.");
+//     }
+//   }
+// }

3d6fdcc3f37f3ba15f4b19f2e47789b94acfe81b
Mesach Venegas Santana <99062079+MesachVenegas@users.noreply.github.com>
2024-06-13 01:26:25 -0700

    add: general types

---
+++ types/index.ts
@@ -0,0 +1,3 @@
+export type Iconkeys = 'home' | 'drop' | 'wallet' | 'card' | 'file' | 'car' | 'lightning' | 'water-drop' | 'wifi' | 'tv' | 'food';
+
+export type VariantKeys = 'green' | 'purple' | 'info' | 'error' | 'neutral' | 'warning';
